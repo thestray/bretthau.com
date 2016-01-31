1cfd5ee1f6d246bf5ab95c7df85d5ced8f0276a6
Brett Hau <Brett Hau>
2016-01-31 21:30:57 +0900

    updating build locations

---
+++ Gruntfile.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt) {
+ // Load grunt plugins.
+ grunt.loadNpmTasks('grunt-contrib-stylus');
+ grunt.loadNpmTasks('grunt-contrib-jade');
+ grunt.loadNpmTasks('grunt-contrib-watch');
+    
+};
