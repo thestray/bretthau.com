d583328cfec87bad8a7f32504c5e642367dea057
thestray <brett.hau@gmail.com>
2016-01-31 22:59:02 +0900

    tidy code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,42 @@
-module.exports = function(grunt) {
- 
-grunt.initConfig({
+module.exports = function (grunt) {
 
-stylus: {
-  compile: {
-    options: {
-      compress: false,
-      paths: ['css']
-    },
-    files: {
-      'build/css/bh.css': 'css/bh.styl'
-    }
-  }
-},
-jade: {
-  compile: {
-    files: [{
-      expand: true,
-      cwd: "",
-      src: "*.jade",
-      dest: "build",
-      ext: ".html"
-    }]
-  }
-},
- watch: {
-   jade: {
-    files: [ '*.jade' ],
-    tasks: ['jade']
-  }
-},
- 
-});
-    
- // Load grunt plugins
- grunt.loadNpmTasks('grunt-contrib-stylus');
- grunt.loadNpmTasks('grunt-contrib-jade');
- grunt.loadNpmTasks('grunt-contrib-watch');
- grunt.registerTask('default', ['stylus', 'jade',]);
-    
+    grunt.initConfig({
+
+        stylus: {
+            compile: {
+                options: {
+                    compress: false,
+                    paths: ['css']
+                },
+                files: {
+                    'build/css/bh.css': 'css/bh.styl'
+                }
+            }
+        },
+        jade: {
+            compile: {
+                files: [{
+                    expand: true,
+                    cwd: "",
+                    src: "*.jade",
+                    dest: "build",
+                    ext: ".html"
+                }]
+            }
+        },
+        watch: {
+            jade: {
+                files: ['*.jade'],
+                tasks: ['jade']
+            }
+        }
+
+    });
+
+    // Load grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['stylus', 'jade']);
+
 };
