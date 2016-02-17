f3eff5c50e3eed038fcba70b88570ec2a17fbb5b
thestray <brett.hau@gmail.com>
2016-02-17 12:00:25 +0900

    add typwritter animation

--- js/bh-main.js
+++ js/bh-main.js
@@ -83,4 +83,88 @@ $(document).ready(function() {
     pagination(+$(this).attr("data-target"));
   });
 
+
+
+// Text Typewriter animation
+(function ($) {
+      // writes the string
+      function typeString($target, str, cursor, delay, cb) {
+        $target.html(function (_, html) {
+          return html + str[cursor];
+        });
+
+        if (cursor < str.length - 1) {
+          setTimeout(function () {
+            typeString($target, str, cursor + 1, delay, cb);
+          }, delay);
+        }
+        else {
+          cb();
+        }
+      }
+
+      // clears the string
+      function deleteString($target, delay, cb) {
+        var length;
+
+        $target.html(function (_, html) {
+          length = html.length;
+          return html.substr(0, length - 1);
+        });
+
+        if (length > 1) {
+          setTimeout(function () {
+            deleteString($target, 80, cb);
+          }, delay);
+        }
+        else {
+          cb();
+        }
+      }
+
+      // jQuery hook
+      $.fn.extend({
+        teletype: function (opts) {
+          var settings = $.extend({}, $.teletype.defaults, opts);
+
+          return $(this).each(function () {
+            (function loop($tar, idx) {
+              // type
+              typeString($tar, settings.text[idx], 0, settings.delay, function () {
+                // delete
+                setTimeout(function () {
+                  deleteString($tar, settings.delay, function () {
+                    loop($tar, (idx + 1) % settings.text.length);
+                  });
+                }, settings.pause);
+              });
+
+            }($(this), 0));
+          });
+        }
+      });
+
+      // plugin defaults
+    $.extend({
+      teletype: {
+        defaults: {
+          delay: 100,
+          pause: 3000,
+          text: [],
+          forever: false,
+          end: $.noop
+        }
+      }
+    });
+    }(jQuery));
+
+    $('#target').teletype({
+      text: [
+        'Jade',
+        'Stylus',
+        'JavaScript',
+        'HTML',
+        'CSS'
+      ]
+    });
 });
